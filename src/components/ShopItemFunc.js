2b1448651150fc2259353f761bbd2fa4a7c5a36f
Aleksahappy <aleksa.happy@gmail.com>
2021-05-18 00:22:07 +0400

    homework done

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function ShopItemFunc({item}) {
+  const {brand, title, description, descriptionFull, price, currency} = item;
+  return (
+    <div className="main-content">
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+      <div className="description">{descriptionFull}</div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{currency}{price ? price.toFixed(2) : '0.00'}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+}
+
+ShopItemFunc.propTypes = {
+  item: PropTypes.shape({
+    brand: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    descriptionFull: PropTypes.string,
+    price: PropTypes.number.isRequired,
+    currency: PropTypes.string.isRequired
+  }).isRequired
+};
